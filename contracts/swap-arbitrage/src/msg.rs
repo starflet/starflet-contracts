ee216600aa1b0cf236fbb6e2ca313eb7a4a385b4
JoowonYun <joowon@starflet.io>
2022-01-21 17:36:07 +0900

    feat: support to multiple dex

--- contracts/swap-arbitrage/src/msg.rs
+++ contracts/swap-arbitrage/src/msg.rs
@@ -10,7 +10,7 @@ pub struct InstantiateMsg {
     pub asset_info: AssetInfo,
     pub symbol: String,
     pub token_code_id: u64,
-    pub pair_contract: String,
+    pub pairs: Vec<Pair>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -21,22 +21,27 @@ pub enum ExecuteMsg {
         owner: Option<String>,
         commission_rate: Option<Decimal256>,
         code_id: Option<u64>,
-        pair_contract: Option<String>,
+        add_pairs: Option<Vec<Pair>>,
+        remove_pairs: Option<Vec<String>>,
     },
     Bond {
         asset: Asset,
     },
     Swap {
-        path: Path,
+        path: String,
     },
     Claim {},
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub enum Path {
-    NativeSwapToTerraSwap,
-    TerraSwapToNativeSwap,
+pub struct Pair {
+    pub name: String,
+    pub pair_addr: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Pairs {
+    pub pairs: Vec<Pair>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -46,10 +51,10 @@ pub struct ConfigResponse {
     pub asset_info: AssetInfo,
     pub token_code_id: u64,
     pub token_address: String,
-    pub pair_contract: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
 pub struct MigrateMsg {
-    pub pair_contract: String,
+    pub pairs: Vec<Pair>,
 }
--- contracts/swap-arbitrage/src/state.rs
+++ contracts/swap-arbitrage/src/state.rs
@@ -1,20 +1,61 @@
+use cosmwasm_std::{Addr, Deps, DepsMut, Order, StdResult};
+use cw_storage_plus::{Bound, Item, Map};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{Addr, Deps, DepsMut, StdResult};
-use cw_storage_plus::Item;
+pub const PAIRS: Map<String, Addr> = Map::new("pairs");
+
+pub fn get_pair(deps: Deps, name: String) -> StdResult<Addr> {
+    PAIRS.load(deps.storage, name)
+}
+
+pub fn set_pair(deps: DepsMut, name: String, pair: Addr) -> StdResult<()> {
+    PAIRS.save(deps.storage, name, &pair)
+}
+
+pub fn remove_pair(deps: DepsMut, name: String) {
+    PAIRS.remove(deps.storage, name)
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct Contracts {
-    pub pair: Addr,
+pub struct PairInfo {
+    pub name: String,
+    pub pair_addr: Addr,
 }
 
-pub const CONTRACTS: Item<Contracts> = Item::new("contracts");
+const MAX_LIMIT: u32 = 30;
+const DEFAULT_LIMIT: u32 = 10;
+pub fn load_pairs(
+    deps: Deps,
+    start_after: Option<String>,
+    limit: Option<u32>,
+) -> StdResult<Vec<PairInfo>> {
+    let limit = limit.unwrap_or(DEFAULT_LIMIT).min(MAX_LIMIT) as usize;
 
-pub fn get_contracts(deps: Deps) -> StdResult<Contracts> {
-    CONTRACTS.load(deps.storage)
+    let start = start_after.map(|s| Bound::exclusive(s.as_bytes().to_vec()));
+
+    PAIRS
+        .range(deps.storage, start, None, Order::Ascending)
+        .take(limit)
+        .map(|item| {
+            let (k, v) = item?;
+            Ok(PairInfo {
+                name: String::from_utf8(k).unwrap(),
+                pair_addr: v,
+            })
+        })
+        .collect::<StdResult<Vec<PairInfo>>>()
 }
 
-pub fn set_contracts(deps: DepsMut, contracts: Contracts) -> StdResult<()> {
-    CONTRACTS.save(deps.storage, &contracts)
+pub const TMP_SWAP: Item<String> = Item::new("tmp_swap");
+pub fn get_tmp_swap(deps: Deps) -> StdResult<String> {
+    TMP_SWAP.load(deps.storage)
+}
+
+pub fn set_tmp_swap(deps: DepsMut, swap: String) -> StdResult<()> {
+    TMP_SWAP.save(deps.storage, &swap)
+}
+
+pub fn remove_tmp_swap(deps: DepsMut) {
+    TMP_SWAP.remove(deps.storage)
 }
