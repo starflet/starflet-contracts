81b62899c3ed271ae6713ff8744c2e07765c7a14
JoowonYun <joowon@starflet.io>
2022-01-29 23:00:35 +0900

    feat: use router & recursively up to the minimum amount

--- contracts/swap-arbitrage/src/state.rs
+++ contracts/swap-arbitrage/src/state.rs
@@ -1,7 +1,17 @@
-use cosmwasm_std::{Addr, Deps, DepsMut, Order, StdResult};
+use cosmwasm_std::{Addr, Deps, DepsMut, Order, StdResult, Uint128};
 use cw_storage_plus::{Bound, Item, Map};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+
+pub const ROUTER: Item<Addr> = Item::new("router");
+
+pub fn get_router(deps: Deps) -> StdResult<Addr> {
+    ROUTER.load(deps.storage)
+}
+
+pub fn set_router(deps: DepsMut, router: Addr) -> StdResult<()> {
+    ROUTER.save(deps.storage, &router)
+}
 
 pub const PAIRS: Map<String, Addr> = Map::new("pairs");
 
@@ -47,13 +57,25 @@ pub fn load_pairs(
         .collect::<StdResult<Vec<PairInfo>>>()
 }
 
-pub const TMP_SWAP: Item<String> = Item::new("tmp_swap");
-pub fn get_tmp_swap(deps: Deps) -> StdResult<String> {
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct TmpSwap {
+    pub route_path: String,
+    pub minimum_receive: Uint128,
+}
+
+pub const TMP_SWAP: Item<TmpSwap> = Item::new("tmp_swap");
+pub fn get_tmp_swap(deps: Deps) -> StdResult<TmpSwap> {
     TMP_SWAP.load(deps.storage)
 }
 
-pub fn set_tmp_swap(deps: DepsMut, swap: String) -> StdResult<()> {
-    TMP_SWAP.save(deps.storage, &swap)
+pub fn set_tmp_swap(deps: DepsMut, route_path: String, minimum_receive: Uint128) -> StdResult<()> {
+    TMP_SWAP.save(
+        deps.storage,
+        &TmpSwap {
+            route_path,
+            minimum_receive,
+        },
+    )
 }
 
 pub fn remove_tmp_swap(deps: DepsMut) {
